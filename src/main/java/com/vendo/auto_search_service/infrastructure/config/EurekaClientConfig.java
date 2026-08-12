ee96ab4b31a7c1cbbd377bdf0d1749556996f94d
HaaaamBurger <gemasclashes@gmail.com>
2026-08-12 17:04:07 +0300

    version downcasting to spring 3.5.6

--- src/main/java/com/vendo/auto_search_service/infrastructure/config/EurekaClientConfig.java
+++ src/main/java/com/vendo/auto_search_service/infrastructure/config/EurekaClientConfig.java
@@ -1,4 +1,4 @@
-package com.vendo.auto_search_service;
+package com.vendo.auto_search_service.infrastructure.config;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
