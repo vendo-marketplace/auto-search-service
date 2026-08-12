d1747aed0b872b29e30b7c733667a0f0d772f442
HaaaamBurger <gemasclashes@gmail.com>
2026-08-12 15:43:00 +0300

    init

---
+++ src/main/java/com/vendo/auto_search_service/AutoSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vendo.auto_search_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoSearchServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AutoSearchServiceApplication.class, args);
+	}
+
+}
