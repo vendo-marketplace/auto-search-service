982a21b13def1ff12d5298ea7b505a9ede4213a8
HaaaamBurger <gemasclashes@gmail.com>
2026-08-12 16:44:23 +0300

    refactoring

---
+++ src/main/java/com/vendo/auto_search_service/EurekaClientConfig.java
@@ -0,0 +1,9 @@
+package com.vendo.auto_search_service;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class EurekaClientConfig {
+}
