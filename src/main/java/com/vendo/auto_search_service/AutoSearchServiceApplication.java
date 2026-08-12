27462f85900c240f23da1e38b3f905a40e99b264
HaaaamBurger <gemasclashes@gmail.com>
2026-08-12 18:00:18 +0300

    refactoring

--- src/main/java/com/vendo/auto_search_service/AutoSearchServiceApplication.java
+++ src/main/java/com/vendo/auto_search_service/AutoSearchServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AutoSearchServiceApplication {
 
-	static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(AutoSearchServiceApplication.class, args);
 	}
 
