0033fbb11cf33e063ac15c9b68723dd382a9b0ae
HaaaamBurger <gemasclashes@gmail.com>
2026-09-26 17:55:35 +0200

    kan-114: find auto search by city

--- src/main/java/com/vendo/auto_search_service/adapter/auto_search/in/dto/AutoSearchResponse.java
+++ src/main/java/com/vendo/auto_search_service/adapter/auto_search/in/dto/AutoSearchResponse.java
@@ -2,6 +2,7 @@ package com.vendo.auto_search_service.adapter.auto_search.in.dto;
 
 import com.vendo.auto_search_service.domain.auto_search.nested.Owner;
 import com.vendo.auto_search_service.domain.auto_search.type.SearchStatus;
+import com.vendo.auto_search_service.shared.Address;
 
 import java.math.BigDecimal;
 import java.time.Instant;
@@ -9,13 +10,17 @@ import java.time.LocalDateTime;
 
 public record AutoSearchResponse(
         String id,
-        String categoryId,
+
         Owner owner,
+
+        String categoryId,
         BigDecimal minPrice,
         BigDecimal maxPrice,
-        String address,
+        Address address,
+
         SearchStatus status,
         LocalDateTime expirationDate,
+
         Instant createdAt,
         Instant updatedAt
 ) {
